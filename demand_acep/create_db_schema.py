747449f7d8b611172f8a0d55258a663e2386f64b
C. Pathak <cp84@n2232.hyak.local>
2019-05-02 19:30:04 -0700

    WIP schema creation script

---
+++ demand_acep/create_db_schema.py
@@ -0,0 +1,29 @@
+""" This file contains functions that create the schema in the database """
+
+from sqlalchemy import create_engine  
+from sqlalchemy import Table, Column, String, MetaData
+import pandas as pd
+
+def read_source_file(file_path, file_name):
+    """ 
+    This function reads the source file and extracts the table names and
+    corresponding column names.
+    
+    Parameters
+    ----------
+    file_path : string
+        `file_path` represents the relative path of the source file.
+    file_name : string
+        The parameter `file_name` contains the name of the file containing the 
+        imformation about the meter names and channels monitored etc.
+    
+    Returns
+    -------
+    int
+        Description of anonymous integer return value.
+    """
+    
+    source_file
+    
+    return 
+    
--- demand_acep/tests/test_demand_acep.py
+++ demand_acep/tests/test_demand_acep.py
@@ -15,14 +15,12 @@ import pytest # automatic test finder and test runner
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import demand_acep as da
-from demand_acep import hello_world
+from create_db_schema import read_source_file 
 
-def test_hello_world():
-    """
-    This function test the function hello_world()
-    """
-    result = hello_world()
-
-    assert result == "Hello world!", "The function does not return the string 'Hello world!'"
-
-    return
+################################################################################
+######## Tests for function read_source_file ###################################
+################################################################################
+def test_read_source_file():
+    
+    
+    return 
