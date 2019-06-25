df2d35faddbafac65be92e5cde582b9b673b1938
Yohan Min <min25@uw.edu>
2019-06-24 21:48:44 -0700

    adding r instruction

---
+++ demand/R/demand.R
@@ -0,0 +1,7 @@
+#' demand package
+#'
+#' This package creates diverse plots per day, weekday, month and year for peak demand power consumption of several meters. These plots lead to benefit-cost analyses and cost saving plots. In addition, this package forecasts peak power demand using ARIMA on a daily and monthly basis. Correlation and a simple regression are also included.
+#'
+#' @docType package
+#' @name demand
+NULL
